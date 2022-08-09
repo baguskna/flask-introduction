5222c6381f1587eabf3218065547e5ca452b60d4
Bagus Kurnia <bagus.kurnia@sirclo.com>
2022-08-09 09:28:15 +0700

    feat: setup database

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)  # initialize the database
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
